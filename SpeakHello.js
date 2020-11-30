b71dafc1ba0b766393bc91e1cb0e603ab8f738bc
akashsajjan <54077132+akashsjjan@users.noreply.github.com>
2020-11-30 20:07:17 +0530

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window) {
+    var helloSpeaker = new Object();
+    var speakWord = "hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
